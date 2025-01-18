dcf26da0be57c381ecb558b265111a613ef0df66
joe@Pi <joe288@live.de>
2025-01-18 17:24:37 +0100

    add captureSmal

--- main.py
+++ main.py
@@ -95,19 +95,14 @@ def openPS():
     dev.set_configuration()
 
     data = dev.ctrl_transfer(0xc0, 0xa0, 0, 0, 3) #returns a000cb
-    # print("read data:", ' '.join(format(byte, '02x') for byte in data))
     data = dev.ctrl_transfer(0xc0, 0x29, 0, 0, 4) #returns 29000300 (probably means "flag value is 3")
-    # print("read data:", ' '.join(format(byte, '02x') for byte in data))
     data = dev.ctrl_transfer(0xc0, 0x66, 0, 0, 3) #returns 660000
-    # print("read data:", ' '.join(format(byte, '02x') for byte in data))
     data = dev.ctrl_transfer(0xc0, 0xa3, 0, 0, 4) #returns a392cd01
-    # print("read data:", ' '.join(format(byte, '02x') for byte in data))
     
     device_name = deviceName()
     print("PalmSecure: Connected to device ", device_name)
         
     data = dev.ctrl_transfer(0xc0, 0x29, 1, 4, 4) #returns 29010400 (probably means "flag value is 4")
-    # print("read data:", ' '.join(format(byte, '02x') for byte in data))
     
     # We initialize device's random generator with this method, it seems
     key = dev.ctrl_transfer(0xc0, 0x35, random.randint(0, 0xffff), random.randint(0, 0xffff), 18 ) #returns 3500 + encryption key
@@ -126,66 +121,51 @@ def openPS():
     if res == -1:
         print("false")
 
-    data = dev.ctrl_transfer(0xc0, 0x29, 1, 0, 4)   #returns 29010400 (probably means something)
-    data = dev.ctrl_transfer(0xc0, 0xf6, 9, 0, 168) #returns a400000002000000d0051000b7071000d7a3703d0ad7fd3fd7a3703d0ad7a33f6666666666565e401c041000f80410003108ac1c5a640040367689eaad81ad3f713d0ad7a3605540e80210004b031000f0a7c64b378901404faf94658863b53f52b81e85eb314e400e0210003e021000fed478e9263102406002b7eee6a9be3fae47e17a144e4540740110008c011000cff753e3a59b024024624a24d1cbc43f0ad7a3703d0a3e40
-    data = dev.ctrl_transfer(0xc0, 0xf6, 8, 0, 152) #returns 94000000010000000000000000000040000000000000324000000000000084400000000000007e40fa7e6abc7493783f0038b1e02533833fccc5bd1f468d54bf00001ae91ded883f00ac11642c67ed3f4ba5ca32f867b0bfc42951a36f36793f34fe9ffa4662f43fe3d40ddaa64bd53f9b186be9afb2d9bf693754c7b3e55240aee258d2929a67bf504164df91a63dbf953ead43900be9bf
-    data = dev.ctrl_transfer(0xc0, 0xf6, 7, 0, 302) #returns 3401000001000000040000000000000061e849533fe226c0190e2d82fc2827c00000000000000000094155c358418b3f60805047ad44513fd6fd521d45ffef3f7eb6d422de042740cdccd212dc4a27c00000000000000000433ed92b619f7b3f0f25c8d5ce9661bf3816387acbffef3f29a96ce37edc26c072cc748eef102740000000000000000082f2c7d86e53933f9a4c9416e67b8a3f77fc4916dbfdef3fac1796de70ed2640e428b29c3ce02640000000000000000033f14bd221639f3ffd970612085827bf5f59a39326fcef3f020004004201f0004201ef004301f5004801f20000000000d2a12dffc12342400901b700f800a6007b01b6008c01a5000a012e01fa003e0180012a0191013b01bff27cd4352453402901d7001d01cb005b01d6006701ca002a010e011f01
+    data = dev.ctrl_transfer(0xc0, 0x29, 1, 0, 4)       #returns 29010400 (probably means something)
+    data = dev.ctrl_transfer(0xc0, 0xf6, 9, 0, 168)     #returns a400000002000000d0051000b7071000d7a3703d0ad7fd3fd7a3703d0ad7a33f6666666666565e401c041000f80410003108ac1c5a640040367689eaad81ad3f713d0ad7a3605540e80210004b031000f0a7c64b378901404faf94658863b53f52b81e85eb314e400e0210003e021000fed478e9263102406002b7eee6a9be3fae47e17a144e4540740110008c011000cff753e3a59b024024624a24d1cbc43f0ad7a3703d0a3e40
+    data = dev.ctrl_transfer(0xc0, 0xf6, 8, 0, 152)     #returns 94000000010000000000000000000040000000000000324000000000000084400000000000007e40fa7e6abc7493783f0038b1e02533833fccc5bd1f468d54bf00001ae91ded883f00ac11642c67ed3f4ba5ca32f867b0bfc42951a36f36793f34fe9ffa4662f43fe3d40ddaa64bd53f9b186be9afb2d9bf693754c7b3e55240aee258d2929a67bf504164df91a63dbf953ead43900be9bf
+    data = dev.ctrl_transfer(0xc0, 0xf6, 7, 0, 302)     #returns 3401000001000000040000000000000061e849533fe226c0190e2d82fc2827c00000000000000000094155c358418b3f60805047ad44513fd6fd521d45ffef3f7eb6d422de042740cdccd212dc4a27c00000000000000000433ed92b619f7b3f0f25c8d5ce9661bf3816387acbffef3f29a96ce37edc26c072cc748eef102740000000000000000082f2c7d86e53933f9a4c9416e67b8a3f77fc4916dbfdef3fac1796de70ed2640e428b29c3ce02640000000000000000033f14bd221639f3ffd970612085827bf5f59a39326fcef3f020004004201f0004201ef004301f5004801f20000000000d2a12dffc12342400901b700f800a6007b01b6008c01a5000a012e01fa003e0180012a0191013b01bff27cd4352453402901d7001d01cb005b01d6006701ca002a010e011f01
 
     # switch off all lights?
-    data = dev.ctrl_transfer(0xc0, 0x27, 0, 0, 6)    #returns 270000000000
-    data = dev.ctrl_transfer(0xc0, 0x27, 0x05, 0, 8) #returns 2705000000000000
-    data = dev.ctrl_transfer(0xc0, 0x27, 0x06, 0, 8) #returns 2706000000000000
-    data = dev.ctrl_transfer(0xc0, 0x27, 0x07, 0, 8) #returns 2707000000000000
-    data = dev.ctrl_transfer(0xc0, 0x27, 0x08, 0, 8) #returns 2708000000000000
-    data = dev.ctrl_transfer(0xc0, 0x27, 0x09, 0, 8) #returns 2709000000000000
-    data = dev.ctrl_transfer(0xc0, 0x27, 0x0a, 0, 8) #returns 270a000000000000
-    data = dev.ctrl_transfer(0xc0, 0x27, 0x0b, 0, 8) #returns 270b000000000000
-    data = dev.ctrl_transfer(0xc0, 0x27, 0x0c, 0, 8) #returns 270c000000000000
+    data = dev.ctrl_transfer(0xc0, 0x27, 0, 0, 6)       #returns 270000000000
+    data = dev.ctrl_transfer(0xc0, 0x27, 0x05, 0, 8)    #returns 2705000000000000
+    data = dev.ctrl_transfer(0xc0, 0x27, 0x06, 0, 8)    #returns 2706000000000000
+    data = dev.ctrl_transfer(0xc0, 0x27, 0x07, 0, 8)    #returns 2707000000000000
+    data = dev.ctrl_transfer(0xc0, 0x27, 0x08, 0, 8)    #returns 2708000000000000
+    data = dev.ctrl_transfer(0xc0, 0x27, 0x09, 0, 8)    #returns 2709000000000000
+    data = dev.ctrl_transfer(0xc0, 0x27, 0x0a, 0, 8)    #returns 270a000000000000
+    data = dev.ctrl_transfer(0xc0, 0x27, 0x0b, 0, 8)    #returns 270b000000000000
+    data = dev.ctrl_transfer(0xc0, 0x27, 0x0c, 0, 8)    #returns 270c000000000000
     print("PalmSecure: init done")
     return True
 
 def start():
     global scan_first
     # switch on light
-    data = dev.ctrl_transfer(0xc0, 0x27, 0x07, 1, 8) # returns 2707000000000000
-    data = dev.ctrl_transfer(0xc0, 0x27, 0x08, 1, 8) # returns 2708000000000000
-    data = dev.ctrl_transfer(0xc0, 0x27, 0x00, 1, 6) # returns 270000280000
+    data = dev.ctrl_transfer(0xc0, 0x27, 0x07, 1, 8)    #returns 2707000000000000
+    data = dev.ctrl_transfer(0xc0, 0x27, 0x08, 1, 8)    #returns 2708000000000000
+    data = dev.ctrl_transfer(0xc0, 0x27, 0x00, 1, 6)    #returns 270000280000
     scan_first = True
 
 def stop():
     # stop light?
-	data = dev.ctrl_transfer(0xc0, 0x27, 0x07, 0, 8) # returns ?
-	data = dev.ctrl_transfer(0xc0, 0x27, 0x08, 0, 8) # returns ?
-	data = dev.ctrl_transfer(0xc0, 0x27, 0x00, 0, 6) # returns ?
-	data = dev.ctrl_transfer(0xc0, 0x45, 0, 0, 3)    #returns 450100
+	data = dev.ctrl_transfer(0xc0, 0x27, 0x07, 0, 8)    #returns ?
+	data = dev.ctrl_transfer(0xc0, 0x27, 0x08, 0, 8)    #returns ?
+	data = dev.ctrl_transfer(0xc0, 0x27, 0x00, 0, 6)    #returns ?
+	data = dev.ctrl_transfer(0xc0, 0x45, 0, 0, 3)       #returns 450100
      
 def bufToImage(buf, w, h):
-    # Erstelle ein leeres Bild mit der Größe w x h und 8-Bit Graustufen
     res = np.zeros((h, w), dtype=np.uint8)
 
-    # Setze die Pixelwerte basierend auf dem buf
     for y in range(h):
         for x in range(w):
             res[y, x] = buf[y * w + x]
-
-    # Optional: Konvertiere das Bild in ein 256-Farben-Bild (Indexed)
-    # OpenCV unterstützt kein Indexed8 direkt, aber wir können eine Graustufen-Darstellung verwenden
-    
-    # # Beispielaufruf
-    # w, h = 100, 100  # Breite und Höhe des Bildes
-    # buf = np.random.randint(0, 256, size=(h * w), dtype=np.uint8)  # Beispiel-Puffer mit zufälligen Werten
-    # image = create_image(w, h, buf)
-
-    # Zeige das Bild an (optional)
-    # cv2.imshow('Image', res)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
     return res
 
-def capture_large():
+def captureLarge():
     global scan_first
     res = []
-    print("Capture!")
+    print("Capture lage!")
     data = dev.ctrl_transfer(0xc0, 0x4e, 0, 0, 3)           #returns 4e0100
     data = dev.ctrl_transfer(0xc0, 0x4e, 1, 0, 3)           #returns 4e0100
     data = dev.ctrl_transfer(0xc0, 0x4e, 2, 0, 3)           #returns 4e0100
@@ -208,12 +188,58 @@ def capture_large():
         dat[i + 120 * 640] ^= mask[i]
     res.append(bufToImage(dat, 640, 480))
 
-    print("Capture 2")
-    data = dev.ctrl_transfer(0xc0, 0x44, 0, 1, 6)           #returns 440100b00400
-    dat = bulk_receive(2,307200)                            #normal picture
-    res.append(bufToImage(dat, 640, 480))
+    # print("Capture 2")
+    # data = dev.ctrl_transfer(0xc0, 0x44, 0, 1, 6)           #returns 440100b00400
+    # dat = bulk_receive(2,307200)                            #normal picture
+    # res.append(bufToImage(dat, 640, 480))
 
-    print("Capture 3")
+    # print("Capture 3")
+    # data = dev.ctrl_transfer(0xc0, 0x4d, 0x78, 240, 5)      #returns 4d01005802
+    # data = dev.ctrl_transfer(0xc0, 0x44, 3, 2, 6)           #returns 440100580200
+    # dat = bulk_receive(2,153600)                            #"4 dots"
+
+    res.append(bufToImage(dat, 640, 240))
+
+    #back to scan mode
+    data = dev.ctrl_transfer(0xc0, 0x27, 7, 1, 8)           #returns 2707000000000000
+    data = dev.ctrl_transfer(0xc0, 0x27, 8, 1, 8)           #returns 2708000000000000
+    data = dev.ctrl_transfer(0xc0, 0x27, 0, 1, 6)           #returns 270000280000
+    scan_first = True
+    
+    return res
+
+def captureSmall():
+    res = []
+    print("Capture small!")
+
+    data = dev.ctrl_transfer(0xc0, 0x4e, 0, 0, 3)           #returns 4e0100
+    data = dev.ctrl_transfer(0xc0, 0x4e, 1, 0, 3)           #returns 4e0100
+    data = dev.ctrl_transfer(0xc0, 0x4e, 2, 0, 3)           #returns 4e0100
+    data = dev.ctrl_transfer(0xc0, 0x4e, 3, 0, 3)           #returns 4e0100
+    data = dev.ctrl_transfer(0xc0, 0x46, 0x7b7, 2, 3)       #returns 460100
+    data = dev.ctrl_transfer(0xc0, 0x47, 0x10, 2, 3)        #returns 470100
+    data = dev.ctrl_transfer(0xc0, 0x49, 0x100, 2, 3)       #returns 490100
+    data = dev.ctrl_transfer(0xc0, 0x4c, 0xc0, 96, 5)       #returns 4c0100f000 - image height?
+    data = dev.ctrl_transfer(0xc0, 0x46, 0xc8, 3, 3)        #returns 460100
+    data = dev.ctrl_transfer(0xc0, 0x47, 0x10, 3, 3)        #returns 470100
+    data = dev.ctrl_transfer(0xc0, 0x49, 0x100, 3, 3)       #returns 490100
+    data = dev.ctrl_transfer(0xc0, 0x42, 0, 258, 3)         #returns 420100
+    data = dev.ctrl_transfer(0xc0, 0x43, 0, 0, 3)           #returns 430100
+    data = dev.ctrl_transfer(0xc0, 0x4c, 0xc0, 96, 5)       #returns 4c0100f000 - image height?
+    data = dev.ctrl_transfer(0xc0, 0x44, 2, 0, 6)           #returns 440100f00000
+
+    print("Capture 4")
+    dat = bulk_receive(2,61440)                             #vein data
+    for i in range(96 * 640):
+        dat[i] ^= mask[i]
+    res.append(bufToImage(dat, 640, 96))
+    
+    print("Capture 5")
+    data = dev.ctrl_transfer(0xc0, 0x44, 2, 1, 6)           #returns 440100f00000
+    dat = bulk_receive(2,61440)                             #normal picture
+    res.append(bufToImage(dat, 640, 96))
+
+    print("Capture 6")
     data = dev.ctrl_transfer(0xc0, 0x4d, 0x78, 240, 5)      #returns 4d01005802
     data = dev.ctrl_transfer(0xc0, 0x44, 3, 2, 6)           #returns 440100580200
     dat = bulk_receive(2,153600)                            #"4 dots"
@@ -247,18 +273,20 @@ def do_detect():
             ok = True
 
         if ok:
-            list_of_images = capture_large()  
+            list_of_images = captureLarge()  
+            # list_of_images = captureSmall()
             cv2.imwrite("capture_1.png", list_of_images[0])
-            cv2.imwrite("capture_2.png", list_of_images[1])
-            cv2.imwrite("capture_3.png", list_of_images[2])
+            # cv2.imwrite("capture_2.png", list_of_images[1])
+            # cv2.imwrite("capture_3.png", list_of_images[2])
+            return list_of_images[0]
 
 if __name__ == "__main__":
     ordner_pfad = os.getcwd()
 
     for datei in os.listdir(ordner_pfad):
         if 'capture' in datei:
-            bild_pfad = os.path.join(ordner_pfad, datei)  # Vollständigen Pfad erstellen
-            os.remove(bild_pfad)  # Bild löschen
+            bild_pfad = os.path.join(ordner_pfad, datei)
+            os.remove(bild_pfad)
 
     openPS()
     start()
